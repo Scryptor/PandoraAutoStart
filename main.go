96ff2de06348c17ff60fa8fb1b4a0a9ec0867bd5
igora <igor.abramyan89@gmail.com>
2023-08-10 21:58:13 +0300

    Добавил токен безопасности

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 // Изменить на свои
 const login = "1079312388"
 const pass = "n8V8xqbF"
+const Token = "af0deccbgcgidddjgnvljitntccddui3jhdinfgjgfjEr"
 
 func main() {
 	// Почему я не храню объект пандоры? Я не знаю, сколько у них может длиться сессия, проще каждый раз переконнект делать, тогда точно будет работать.
@@ -19,23 +20,30 @@ func main() {
 	mux.HandleFunc("/start", HandleVehicleStarter)
 	mux.HandleFunc("/stop", HandleVehicleStopper)
 	// так же можно сделать команду стоп двигатель, проверку баланса и т.п., но моя задача была просто запустить через Алису, поэтому если кому надо, то сам или ко мне за прайс
-	fmt.Println("Сервер запущен по адресу http://127.0.0.1:4443, для запуска машины http://127.0.0.1:4443/start")
+	fmt.Println("Сервер запущен по адресу http://127.0.0.1:4443, для запуска машины http://127.0.0.1:4443/start?token=af0deccbgcgidddjgnvljitntccddui3jhdinfgjgfjEr")
 	log.Fatal("Сервер упал: ", http.ListenAndServe(":4443", mux))
 }
 
 func HandleVehicleStarter(w http.ResponseWriter, r *http.Request) {
-	go PandoraStarter()
-	// Запускает движок, советую добавить какой-нибудь токен, чтобы любой посторонний человек не смог запустить авто, зная просто url
-	w.Header().Set("Content-Type", "application/json")
-	warn, _ := json.Marshal("Отправлена команда на запуск")
-	_, _ = w.Write(warn)
+	if GetQuery(r) {
+		go PandoraStarter()
+		w.Header().Set("Content-Type", "application/json")
+		warn, _ := json.Marshal("Отправлена команда на запуск")
+		_, _ = w.Write(warn)
+		return
+	}
+	w.WriteHeader(403)
 }
 
 func HandleVehicleStopper(w http.ResponseWriter, r *http.Request) {
-	go PandoraStopper()
-	w.Header().Set("Content-Type", "application/json")
-	warn, _ := json.Marshal("Отправлена команда на остновку")
-	_, _ = w.Write(warn)
+	if GetQuery(r) {
+		go PandoraStopper()
+		w.Header().Set("Content-Type", "application/json")
+		warn, _ := json.Marshal("Отправлена команда на остновку")
+		_, _ = w.Write(warn)
+		return
+	}
+	w.WriteHeader(403)
 }
 
 func PandoraStarter() {
@@ -60,4 +68,17 @@ func PandoraStopper() {
 
 	time.Sleep(2 * time.Minute)
 	Pandora.Cansel()
+}
+
+func GetQuery(r *http.Request) bool {
+	if len(r.URL.Query()) > 0 {
+		if tVal, ok := r.URL.Query()["token"]; ok {
+			token := tVal[0]
+			if token != Token {
+				return false
+			}
+			return true
+		}
+	}
+	return false
 }
